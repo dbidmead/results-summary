9e4c525e1f73675b1a257d277f1e83799d9e41a6
Drew Bidmead <dmbidmead@gmail.com>
2023-05-03 12:09:49 -0400

    Now uses JSON document to supply score and icon information

---
+++ index.js
@@ -0,0 +1,62 @@
+import scoreData from './data.json' assert { type: "json" };
+
+const reactionScoreDisplay = document.getElementById("rxn-score");
+const memoryScoreDisplay = document.getElementById("mem-score");
+const verbalScoreDisplay = document.getElementById("ver-score");
+const visualScoreDisplay = document.getElementById("vis-score");
+const overallScoreDisplay = document.getElementById("overall-score");
+
+const reactionImgDisplay = document.getElementById("rxn-img");
+const memoryImgDisplay = document.getElementById("mem-img");
+const verbalImgDisplay = document.getElementById("ver-img");
+const visualImgDisplay = document.getElementById("vis-img");
+
+
+let reactionScore = null;
+let reactionImg = null;
+
+let memoryScore = null;
+let memoryImg = null;
+
+let verbalScore = null;
+let verbalImg = null;
+
+let visualScore = null;
+let visualImg = null;
+
+let avgScore = null;
+
+// console.log(scoreData)
+
+scoreData.forEach((obj) => {
+    switch (obj.category) {
+        case "Reaction":
+            reactionScore = obj.score;
+            reactionImg = obj.icon;
+            break;
+        case "Memory":
+            memoryScore = obj.score;
+            memoryImg = obj.icon;
+            break;
+        case "Verbal":
+            verbalScore = obj.score;
+            verbalImg = obj.icon;
+            break;
+        case "Visual":
+            visualScore = obj.score;
+            visualImg = obj.icon;
+            break;
+    }
+})
+avgScore = Math.round((reactionScore + memoryScore + verbalScore + visualScore) / 4);
+
+reactionScoreDisplay.innerHTML = reactionScore;
+memoryScoreDisplay.innerHTML = memoryScore;
+verbalScoreDisplay.innerHTML = verbalScore;
+visualScoreDisplay.innerHTML = visualScore;
+overallScoreDisplay.innerHTML = avgScore;
+
+reactionImgDisplay.src = reactionImg;
+memoryImgDisplay.src = memoryImg;
+verbalImgDisplay.src = verbalImg;
+visualImgDisplay.src = visualImg;
